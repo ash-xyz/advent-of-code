f9170008e0c386c962fb588dc2f081a7f6148c4f
ash-xyz <ashraf.ali@ucdconnect.ie>
2024-03-18 18:13:17 +0000

    day 7 part 2

--- 2019/day7.go
+++ 2019/day7.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"math"
 	"strings"
 )
@@ -16,10 +15,6 @@ func (d *day7) init() {
 	input := ReadFile("inputs/day7.txt")
 	array := strings.Split(input[0], ",")
 	d.instructions = convertToInt(array)
-}
-
-func (d *day7) part2() int {
-	return -1
 }
 
 func (d *day7) nextPermutation(nums []int) {
@@ -49,6 +44,33 @@ func (d *day7) reverse(nums []int, start, end int) {
 	}
 }
 
+func (d *day7) part2() int {
+	phaseSetting := []int{5, 6, 7, 8, 9}
+	thrust := math.MinInt
+
+	for i := 0; i < 120; i++ {
+		amplifiers := [5]*Machine{{0, true}, {0, true}, {0, true}, {0, true}, {0, true}}
+		output := 0
+		done := false
+
+		for !done {
+			for i, amplifier := range amplifiers {
+				last_output := amplifier.Run(d.instructions, output, phaseSetting[i])
+				if last_output == -1 {
+					done = true
+					break
+				}
+				output = last_output
+			}
+		}
+
+		thrust = max(thrust, output)
+		d.nextPermutation(phaseSetting)
+	}
+
+	return thrust
+}
+
 func (d *day7) part1() int {
 	phaseSetting := []int{0, 1, 2, 3, 4}
 	thrust := math.MinInt
@@ -56,7 +78,7 @@ func (d *day7) part1() int {
 		output := 0
 		d.nextPermutation(phaseSetting)
 		for _, phase := range phaseSetting {
-			machine := &Machine{true}
+			machine := &Machine{0, true}
 			output = machine.Run(d.instructions, output, phase)
 		}
 
@@ -73,6 +95,7 @@ func (d day7) run() (int, int) {
 // -------------------------------------------------------------------------------------------------------------------------
 
 type Machine struct {
+	idx             int // beginning at 0
 	hasNotSeenPhase bool
 }
 
@@ -114,26 +137,33 @@ func (m Machine) getArgs(program []int, index int) (int, int, int, int, int) {
 
 func (m *Machine) Run(instructions []int, input int, phaseNum int) int {
 	program := make([]int, len(instructions))
-	var results []int
 	copy(program, instructions)
 
-	idx := 0
-
 	for {
-		opcode, a1, a2, a3, instruction_length := m.getArgs(program, idx)
+		opcode, a1, a2, a3, instruction_length := m.getArgs(program, m.idx)
 		switch opcode {
 		case ADD:
 			program[a3] = a1 + a2
 		case MULTIPLY:
 			program[a3] = a1 * a2
+		case SAVE:
+			if m.hasNotSeenPhase {
+				program[a1] = phaseNum
+				m.hasNotSeenPhase = false
+			} else {
+				program[a1] = input
+			}
+		case OUTPUT:
+			m.idx += instruction_length
+			return program[a1]
 		case JUMP_IF_TRUE:
 			if a1 != 0 {
-				idx = a2
+				m.idx = a2
 				continue
 			}
 		case JUMP_IF_FALSE:
 			if a1 == 0 {
-				idx = a2
+				m.idx = a2
 				continue
 			}
 		case LESS_THAN:
@@ -148,23 +178,12 @@ func (m *Machine) Run(instructions []int, input int, phaseNum int) int {
 			} else {
 				program[a3] = 0
 			}
-		case SAVE:
-			if m.hasNotSeenPhase {
-				program[a1] = phaseNum
-				m.hasNotSeenPhase = false
-			} else {
-				program[a1] = input
-			}
-		case OUTPUT:
-			results = append(results, program[a1])
-			return program[a1]
 		case TERMINATE:
-			fmt.Println(results)
 			return -1
 		default:
 			panic("Bad Op Code")
 		}
 
-		idx += instruction_length
+		m.idx += instruction_length
 	}
 }
